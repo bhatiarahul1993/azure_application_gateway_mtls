0099d40af1870f7f57659b0810beb42d780bbe61
rahbhati <rahul.bhatia@microsoft.com>
2025-06-22 16:21:49 +0530

    adding poc code

---
+++ headers_app/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/headers', (req, res) => {
+  const forwardedHeaders = Object.fromEntries(
+    Object.entries(req.headers).filter(([key]) => key.startsWith('x-forwarded'))
+  );
+  res.json(forwardedHeaders);
+});
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
